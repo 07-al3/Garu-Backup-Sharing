6da960e79f955c62f306d8e831fa404b86c27b9d
07-al3 <alessandropianta2007@gmail.com>
2025-01-30 19:31:43 +0100

    inizio mob barrel

---
+++ Assets/Scripts/Levels/Mob/BarrelBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BarrelBehaviour : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
