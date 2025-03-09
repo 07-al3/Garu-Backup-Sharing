4b711e241fa20a7798e599e50ab013270464c319
07-al3 <alessandropianta2007@gmail.com>
2025-03-09 17:56:50 +0100

    update boss almost done

--- Assets/Scripts/Levels/Mob/POLLUTIUS/BossBehaviour.cs
+++ Assets/Scripts/Levels/Mob/POLLUTIUS/BossBehaviour.cs
@@ -4,14 +4,24 @@ using UnityEngine.UI;
 
 public class BossBehaviour : MonoBehaviour
 {
-    [SerializeField] private Image bossHealthBar;
-    [SerializeField] private Image bossHealthTotal;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject bossHealthBar;
+    [SerializeField] private GameObject bossHealthTotal;
+    [SerializeField] private GameObject trophy;
+    [SerializeField] private BoxCollider2D[] attack1Collider;
+    [SerializeField] private BoxCollider2D attack2Collider;
+    [SerializeField] private Transform finalePosition;
 
     private Animator anim;
     private bool firstTime;
 
     private void Awake()
     {
+        foreach(BoxCollider2D b in attack1Collider)
+            b.enabled = false;
+
+        attack2Collider.enabled = false;
+
         anim = GetComponent<Animator>();
         firstTime = true;
 
@@ -23,26 +33,60 @@ public class BossBehaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(3.5f);
         anim.SetTrigger("Activate");
-        bossHealthTotal.enabled = true;
-        bossHealthBar.enabled = true;
+
+        bossHealthBar.SetActive(true);
+        bossHealthTotal.SetActive(true);
     }
 
     private IEnumerator waitAndStartAttacking()
     {
         yield return new WaitForSeconds(firstTime ? 5 : 0);
 
-        int rand = Random.Range(0, 1);
+        int rand = Random.Range(0, 2);
         anim.SetTrigger(rand == 0 ? "Attack1" : "Attack2");
 
-        yield return new WaitForSeconds(Random.Range(0, 6) + 4);
+        if(rand == 0)
+            transform.localScale = player.transform.position.x > transform.position.x ? new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y) : new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+
+        yield return new WaitForSeconds(Random.Range(0, 4) + (rand == 0 ? 3 : 5));
 
         firstTime = false;
-        waitAndStartAttacking();
+        StartCoroutine(waitAndStartAttacking());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Projectile")
-            bossHealthBar.fillAmount -= 0.033f;
+            bossHealthBar.GetComponent<Image>().fillAmount -= 0.033f;
+    }
+
+    private IEnumerator waitAndDisable(BoxCollider2D c)
+    {
+        yield return new WaitForSeconds(0.5f);
+        c.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        trophy.SetActive(true);
+        StartCoroutine(waitAndFinaleRoom());
+    }
+
+    private IEnumerator waitAndFinaleRoom()
+    {
+        yield return new WaitForSeconds(2);
+        player.transform.position = new Vector2(finalePosition.position.x, finalePosition.position.y);
+    }
+
+    private void attack1()
+    {
+        attack1Collider[player.transform.position.x > transform.position.x ? 0 : 1].enabled = true;
+        StartCoroutine(waitAndDisable(attack1Collider[player.transform.position.x > transform.position.x ? 0 : 1]));
+    }
+
+    private void attack2()
+    {
+        attack2Collider.enabled = true;
+        StartCoroutine(waitAndDisable(attack2Collider));
     }
 }
