e743e051fa44fa7c17cbe2727488660956989cac
07-al3 <alessandropianta2007@gmail.com>
2025-03-12 18:55:05 +0100

    inizio update livello aria

--- Assets/Scripts/Levels/Garu/ChangeMovement.cs
+++ Assets/Scripts/Levels/Garu/ChangeMovement.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class ChangeMovement : MonoBehaviour
 {
-    [SerializeField] private RuntimeAnimatorController animToChange;
+    [SerializeField] private GameObject hubPlayer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag.Equals("Player"))
         {
-            collision.GetComponent<Animator>().runtimeAnimatorController = animToChange;
-            collision.GetComponent<HubMovement>().enabled = true;
-            collision.GetComponent<PlayerMovement>().enabled = false;
-            collision.GetComponent<Rigidbody2D>().gravityScale = 0;
+            gameObject.SetActive(false);
+            hubPlayer.SetActive(true);
         }
     }
 }
